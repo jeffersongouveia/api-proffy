00858b82f978463e08769777ba892a5e2a9a686a
Jefferson Gouveia <jeffersongouveia@eucatur.com.br>
2020-08-08 20:15:59 -0400

    🔧 Params on /classes now optional

--- src/controllers/ClassesController.ts
+++ src/controllers/ClassesController.ts
@@ -17,24 +17,27 @@ export default class ClassesController {
     const week_day = filters.week_day as string
     const time = filters.time as string
 
-    if (!subject || !week_day || !time) {
-      return response.status(400).json({
-        error: 'Missing filters to search classes'
-      })
-    }
-
-    const timeInMinutes = convertHoursToMinutes(time)
-
     const classes = await db('classes')
       .whereExists(function() {
         this.select('class_schedule.*')
           .from('class_schedule')
           .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
-          .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
-          .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
-          .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
+          .modify((query) => {
+            if (week_day) {
+              query.whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+            }
+            if (time) {
+              const timeInMinutes = convertHoursToMinutes(time)
+              query.whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
+              query.whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
+            }
+          })
       })
-      .where('classes.subject', '=', subject)
+      .modify((query) => {
+        if (subject) {
+          query.where('classes.subject', '=', subject)
+        }
+      })
       .join('users', 'classes.user_id', '=', 'users.id')
       .select(['classes.*', 'users.*'])
 
